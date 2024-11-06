b81b001753bba167db06e3616ee4eff0a551cd12
unknown <yahav3003@gmail.com>
2024-11-07 01:19:29 +0200

    made init queue

--- home1/home1.cpp
+++ home1/home1.cpp
@@ -1,20 +1,16 @@
-// home1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Queue.h"
 
-int main()
+/*
+this Function initialize a queue with a specified capacity
+Input: pointer to a Queue structure, queue size
+Output: none
+*/
+void initQueue(Queue* q, unsigned int size)
 {
-    std::cout << "Hello World!\n";
+
+	q->queue = new unsigned int[size];
+	q->max = size;
+	q->count = 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
