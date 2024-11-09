1c0c41e841894d1da36a9e7b3de663614725297a
unknown <yahav3003@gmail.com>
2024-11-09 02:11:05 +0200

    fixe heder and made add_nm

--- home1-ex2/LinkedList.cpp
+++ home1-ex2/LinkedList.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include "LinkedList.h"
 
+
+nm_stack* add_nm(nm_stack* stack, int nm)
+{
+
+	nm_stack* node = new nm_stack;
+	node->val = nm;
+	node->next = stack;
+
+}
--- home1-ex2/LinkedList.h
+++ home1-ex2/LinkedList.h
@@ -2,12 +2,10 @@ typedef struct nm_stack
 {
 
 	unsigned int val;
-	struct personNode* next;
+	struct nm_stack* next;
 
 } nm_stack;
 
-nm_stack* add_nm(nm_stack stack, int nm);
+nm_stack* add_nm(nm_stack* stack, int nm);
 
-nm_stack* delet(nm_stack stack);
-
-nm_stack creat_node(nm_stack stack, int nm);
+nm_stack* delet(nm_stack* stack);
